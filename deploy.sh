d3ca10abaab12cf95c0e054e2d77bf2fc5210dad
Glen Horton <glen.horton@gmail.com>
2014-08-05 15:10:43 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ rm -rf curate_uc
 echo "Cloning Curate_UC..."
 git clone https://github.com/uclibs/curate_uc.git
 cd /srv/apps/curate_uc
-git checkout develop
+git checkout vagrant
 
 echo "Running bundler..."
 export PATH=$PATH:/srv/apps/.gem/ruby/2.1.0/bin
 gem install --user-install bundler
 bundle install --path vendor/bundle
 
-echo "Adjusting config files..."
-sed -i.bak s/ucdbdil7\.private/localhost/g /srv/apps/curate_uc/config/database.yml
-sed -i.bak s/curate-devdb\.uc\.edu/data\.local\:8080/g /srv/apps/curate_uc/config/solr.yml
-sed -i.bak s/curate-devdb\.uc\.edu/data\.local\:8080/g /srv/apps/curate_uc/config/fedora.yml
+# echo "Adjusting config files..."
+# sed -i.bak s/ucdbdil7\.private/localhost/g /srv/apps/curate_uc/config/database.yml
+# sed -i.bak s/curate-devdb\.uc\.edu/data\.local\:8080/g /srv/apps/curate_uc/config/solr.yml
+# sed -i.bak s/curate-devdb\.uc\.edu/data\.local\:8080/g /srv/apps/curate_uc/config/fedora.yml
 
 echo "Running database migrations..."
 bundle exec rake db:migrate
