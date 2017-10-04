770c8d42ee6f49b5af6383fa3e06d219c26bfb61
Glen Horton <hortongn@ucmail.uc.edu>
2017-10-04 16:37:25 -0400

    Update for Scholar 3

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,25 @@ cd /srv/apps
 rm -rf curate_uc
 
 echo "Cloning Curate_UC..."
-git clone https://github.com/uclibs/curate_uc.git
+git clone https://github.com/uclibs/scholar_uc.git curate_uc
 cd /srv/apps/curate_uc
-git checkout vagrant
+git checkout develop
+
+mkdir tmp
+chmod +x script/*.sh
+./script/copy_config_local.sh
 
 echo "Running bundler..."
-export PATH=$PATH:/srv/apps/.gem/ruby/2.1.0/bin
+export PATH=$PATH:/srv/apps/.gem/ruby/2.4.0/bin
 gem install --user-install bundler
 bundle install --path vendor/bundle
 
 echo "Running database migrations..."
 bundle exec rake db:migrate
 
-echo "Starting resque pool..."
-export PATH=$PATH:/opt/fits/fits-0.6.2/
-chmod +x /srv/apps/curate_uc/script/restart_resque.sh
-./script/restart_resque.sh development
+echo "Starting Sidekiq..."
+./script/restart_sidekiq.sh development
+
+export PATH=$PATH:/opt/fits/fits-0.8.5/
+touch tmp/restart.txt
+
